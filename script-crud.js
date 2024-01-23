e7beea8120345d30b1133329951d8c99355df57b
Bruno Boaventura Martins <bruno.bm0501@hotmail.com>
2024-01-23 17:36:48 -0300

    Atualização lógica tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,9 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const btnCancelarTarefa = document.querySelector('.app__form-footer__button--cancel');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
-let edicaoAtiva = false;
+
 
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];//ao invés de iniciar com um array vazio ele já verifica no inicio se tem algo guardado na localstorage caso não tenha, vai pro array vazio. É preciso converter a string de volta em um array.
@@ -15,74 +16,71 @@ function atualizarTarefas (){
 
 
 
-function criarElementoTarefa (tarefa){
+function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
-
+  
     const svg = document.createElement('svg');
     svg.innerHTML = `
-    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-    </svg>
-`;
-
+      <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+          <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+      </svg>
+  `;
+  
     const paragrafo = document.createElement('p');
     paragrafo.textContent = tarefa.descricao;
     paragrafo.classList.add('app__section-task-list-item-description');
-
+    paragrafo.contentEditable = false;
+  
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
-
+  
     function salvarOuEditar() {
-        
-        const textoAnterior = tarefa.descricao; // Salva o texto anterior
-
-        if (edicaoAtiva) {
-            // Salvar mudanças
-            const novaDescricao = paragrafo.textContent.trim();
-            if (novaDescricao !== ''){
-                paragrafo.contentEditable = false;
-                paragrafo.textContent = novaDescricao;
-                tarefa.descricao = novaDescricao;
-                atualizarTarefas();
-            }else{
-                paragrafo.textContent = textoAnterior;
-                alert('Digite algo');
-                paragrafo.focus();
-                return;
-            }
-            
-        } else {
-            // Permitir edição
-            paragrafo.contentEditable = true;
-            paragrafo.focus();
+      if (paragrafo.contentEditable === 'false') {
+        paragrafo.contentEditable = true;
+        paragrafo.focus();
+      } else {
+        paragrafo.contentEditable = false;
+        const novaDescricao = paragrafo.textContent.trim();
+  
+        if (novaDescricao === '') {
+          paragrafo.contentEditable = true;
+          paragrafo.focus();
+          return;
         }
-    
-        edicaoAtiva = !edicaoAtiva;
-        console.log(edicaoAtiva);
+  
+        paragrafo.textContent = novaDescricao;
+        tarefa.descricao = novaDescricao;
+        atualizarTarefas();
+      }
     }
+  
     botao.onclick = salvarOuEditar;
-
-    paragrafo.addEventListener('keydown', (event) => {// opção para utilizar o enter para salvar
-    if (event.key === 'Enter') {
+  
+    paragrafo.addEventListener('keydown', (event) => {
+      // opção para utilizar o enter para salvar
+      if (event.key === 'Enter') {
         event.preventDefault();
         salvarOuEditar();
-        }
+      }
     });
-
-
+  
+    paragrafo.addEventListener('blur', () => {
+      salvarOuEditar();
+    });
+  
     const imagemBotao = document.createElement('img');
-
-    imagemBotao.setAttribute('src','/imagens/edit.png');
-    botao.append(imagemBotao);//colocando a imagem dentro do botão
-
+  
+    imagemBotao.setAttribute('src', '/imagens/edit.png');
+    botao.append(imagemBotao); //colocando a imagem dentro do botão
+  
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
-
+  
     return li;
-}
+  }
 
 btnAdicionarTarefa.addEventListener('click', () =>{
     formAdicionarTarefa.classList.toggle('hidden');// se tem a classe ele tira, se não ele adiciona.
@@ -98,6 +96,12 @@ formAdicionarTarefa.addEventListener('submit', (evento)=> {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
     atualizarTarefas();
+    textarea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+
+})
+
+btnCancelarTarefa.addEventListener('click', () =>{
     textarea.value = '';
     formAdicionarTarefa.classList.add('hidden');
 
